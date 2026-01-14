32bc3c27df1e24ed27196839a7e9be6e190f182c
yosel1989 <43157286+yosel1989@users.noreply.github.com>
2026-01-13 19:43:22 -0500

    Delete src/environments directory

--- src/environments/environment.development.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: false,
-    authUrl: 'https://www.genexs.pe/api/seguridad/api/authentication',
-    //apiUrl: 'https://api.loteriatiorico.com/api/v1',
-    apiUrl: 'https://localhost:44304/api/v1',
-    socket: 'https://localhost:44304',
-};
